a6280affa4f9c8358a7e8b9124ab66feaa3cf438
Pablo Perez <paperezpablo@gmail.com>
2024-10-01 08:13:36 -0300

    Polimorfismo-MetodosPago

--- ejercicio2/Main.java
+++ ejercicio2/Main.java
@@ -23,7 +23,17 @@ public class Main{
         MetodoPago paypal = new Paypal();
 
         //Procesamos pagos con diferentes metodos
-        TarjetaCredito.procesarPago(100);
-        paypal.procesarPago(325);
+        //TarjetaCredito.procesarPago(100);
+        //paypal.procesarPago(325);
+
+        MetodoPago[] metodosPagos = {TarjetaCredito ,paypal };
+
+        // Monto a pagar
+        double[] montos = { 150, 200};
+
+        // Procesamos pagos
+        for (int i = 0; i < metodosPagos.length; i++) {
+            metodosPagos[i].procesarPago(montos[i]);            
+        }
     }
 }
